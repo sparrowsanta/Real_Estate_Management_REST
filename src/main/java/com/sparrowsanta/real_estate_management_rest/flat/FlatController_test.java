35c2b176245f1eac8e761aeec0283989bffdc955
Jakub Wrobel <jakubwrobel22@gmail.com>
2020-08-27 14:46:36 +0200

    Generic implementation - all classes, adding Swagger implementation

--- src/main/java/com/sparrowsanta/real_estate_management_rest/flat/FlatController_test.java
+++ src/main/java/com/sparrowsanta/real_estate_management_rest/flat/FlatController_test.java
@@ -5,7 +5,6 @@ import com.sparrowsanta.real_estate_management_rest.client.ClientService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
